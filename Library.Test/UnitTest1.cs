2ea9a1597c29c77a78aa9c6c771cf65c7ea446ae
Timur <wargerun@gmail.com>
2019-05-08 22:45:02 +0300

    feat: 1. changed project platform 2. added file in gitignore for resharper

---
+++ Library.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Library.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TODO TDD development?
+            Assert.AreEqual(true, true);
+        }
+    }
+}
